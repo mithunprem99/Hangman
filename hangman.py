6cdea35984cd2d73ef3b41ae5ec236a99b6c7901
mithunprem99 <mithunprem1234@gmail.com>
2023-05-29 14:50:27 +0530

    Green: Masked word no repetation

---
+++ hangman.py
@@ -0,0 +1,21 @@
+import random
+
+
+def get_random_word(wordfile = "/usr/share/dict/words"):
+    candidate_words = []
+    with open(wordfile) as f:
+        for word in f:
+            word = word.strip()
+            if len(word) >= 6 and word.islower() and word.isalpha():
+                candidate_words.append(word)
+    word = random.choice(candidate_words)
+    return word
+
+def masked_word(guess_word,worng_guess):
+    return '-'*len(guess_word)
+       
+
+
+
+
+
---
+++ wordlist.py
@@ -0,0 +1,12 @@
+hanglist=[
+    'sergeanty',
+    'amphipneustic',
+    'hypochil',
+    'grus',
+    'depressed',
+    'distortedly',
+    'unselected',
+    'crakow',
+    'preendorsing',
+    'convenient'
+]
