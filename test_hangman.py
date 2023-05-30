7db37b9a8c97cf89454001f382620ee8f59fd780
mithunprem99 <mithunprem1234@gmail.com>
2023-05-30 15:31:48 +0530

    GREEN: Check_correct_word passed away

--- test_hangman.py
+++ test_hangman.py
@@ -102,15 +102,15 @@ def test_check_already_guessed():
 
 def test_check_correct_word():
     word = "elephant"
-    guesses = ["a", "t"]
-    turns_remaining = 6
-    new_guess = "i"
-    status, remaining_turns = hangman.check(word, guesses, 
+    guesses = ["l", "t"]
+    remaining_turns = 6
+    new_guess = "a"
+    status, remaining_turns = hangman.check_guesses(word, guesses, 
                                             remaining_turns, 
                                             new_guess)
     assert status == hangman.Correct
     assert remaining_turns == 6
-    assert guesses == ["a", "t", "i"]
+    assert guesses == ["l", "t", "a"]
 
 
 # def test_check_game_win():
